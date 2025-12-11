46e5519ee756c5d36aca8fe5979d6a2e588009a5
Martynas Gecas <martynas19949@gmail.com>
2025-12-11 18:27:55 +0000

    Fix Google OAuth callback URL configuration

--- src/config/passport.js
+++ src/config/passport.js
@@ -32,6 +32,7 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
         callbackURL: `${
           process.env.BACKEND_URL || "http://localhost:4000"
         }/api/auth/google/callback`,
+        passReqToCallback: false,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
